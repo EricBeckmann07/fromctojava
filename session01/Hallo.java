d61091f07ab271c1947e7a9a42ae3bd93a4d2589
EricBeckmann07 <eric.uwe.beckmann@gmail.com>
2026-06-05 08:50:32 +0000

    dcw

--- session01/Hallo.java
+++ session01/Hallo.java
@@ -1,10 +1,23 @@
+import java.util.ArrayList; 
+import java.util.List; 
+
 public class Hallo {
     public static void main(String[] args) {
-        System.out.println("Hallo Java!");
-        int zahl = 41;
-        System.out.println("Die Zahl: " + zahl);
-        double temp = 19.3;
-        System.out.println("Temp: " + temp);
-        System.err.println("Das ist eine Fehlermeldung");
+        List<SensorReading> readings = new ArrayList<>(); 
+
+        
+
+        // Element hinzufügen: 
+        readings.add(reading); 
+
+        
+
+        // Größe abfragen: 
+        int count = readings.size(); 
+
+        
+
+        // Element an Index abrufen: 
+        SensorReading first = readings.get(0); 
     }
 }
