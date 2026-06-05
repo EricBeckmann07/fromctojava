c90db6a77e39c2460be7fd56126ba7d2928ae558
EricBeckmann07 <eric.uwe.beckmann@gmail.com>
2026-06-05 10:05:53 +0000

    dfcr

--- session01/Hallo.java
+++ session01/Hallo.java
@@ -1,23 +1,19 @@
 import java.util.ArrayList; 
 import java.util.List; 
+import java.util.Iterator; 
 
 public class Hallo {
     public static void main(String[] args) {
-        List<SensorReading> readings = new ArrayList<>(); 
 
-        
+        List<Describable> things = new ArrayList<>(); 
 
-        // Element hinzufügen: 
-        readings.add(reading); 
+        things.add(new SensorReading2(1, "S1", 19.3, 64.2f)); 
+        things.add(new Station("Nord", "Freiburg")); 
+        things.add(new SensorReading2(2, "S2", 22.1, 61.0f)); 
+        things.add(new Station("Süd", "Basel"));
 
-        
-
-        // Größe abfragen: 
-        int count = readings.size(); 
-
-        
-
-        // Element an Index abrufen: 
-        SensorReading first = readings.get(0); 
+        for (Describable d : things) {
+            System.out.println(d.describe());
+        }
     }
 }
